df557b037ac6b7eb2ea2baa9d0d2b8eff5bafca2
Virendra Carpenter <66617140+virendracarpenter@users.noreply.github.com>
2022-02-24 14:25:56 +0530

    initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,39 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->pushButton_0, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_1, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_2, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_3, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_4, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_5, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_6, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_7, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_8, SIGNAL(released()), this, SLOT(digit_pressed()));
+    connect(ui->pushButton_9, SIGNAL(released()), this, SLOT(digit_pressed()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::digit_pressed()
+{
+    QPushButton * button = (QPushButton*)sender();
+
+    double lableNumber;
+    QString newLable;
+    lableNumber = (ui->label->text() + button->text()).toDouble();
+
+    newLable = QString::number(lableNumber,'g', 15);
+
+    ui->label->setText(newLable);
+}
+
